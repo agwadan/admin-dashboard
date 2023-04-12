4dad49796428437ae13a1d087a50279fef5ba73d
agwadan <agwa.dan@gmail.com>
2023-04-12 17:12:19 +0300

    Update import for app.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-
+      <h1>Dashboard</h1>
     </div>
   )
 }
