0c4fceeac9c135aa2a9f165dd67fcdc1aad3474b
agwadan <agwa.dan@gmail.com>
2023-04-20 10:42:42 +0300

    Complete theme pages

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,8 @@
 import { createContext, useState, useMemo } from "react";
 import { createTheme } from "@mui/material/styles";
 
+
+//Dark and light mode theme settings
 export const tokens = (mode) => ({
   ...(mode === "dark" ? {
     grey: {
@@ -123,4 +125,99 @@ export const tokens = (mode) => ({
       900: "#e1e2fe",
     }
   })
-})
+});
+
+//Material UI theme settings
+export const themeSettings = (mode) => {
+  const colors = tokens(mode); //-------------------- The tokens function is called and the mode value is passed in.
+
+  return {
+    palette: {
+      mode: mode,
+      ...(mode === "dark"
+        ? {
+          primary: {
+            main: colors.primary[500]
+          },
+          secondary: {
+            main: colors.greenAccent[500],
+          },
+          neutral: {
+            dark: colors.grey[700],
+            main: colors.grey[500],
+            light: colors.grey[100],
+          },
+          background: {
+            default: colors.primary[500],
+          },
+        } : {
+          primary: {
+            main: colors.primary[100],
+          },
+
+          secondary: {
+            main: colors.greenAccent[500],
+          },
+
+          neutral: {
+            dark: colors.grey[700],
+            main: colors.grey[500],
+            light: colors.grey[100],
+          },
+
+          background: {
+            default: "#fcfcfc",
+          },
+        })
+    },
+
+    typography: {
+      fontFamily: ["Source Sans Pro", "sans-serif"].join(","),//------- The comma is used to join the two font families
+      fontSize: 12,
+      h1: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 40,
+      },
+      h2: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 32,
+      },
+      h3: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 24,
+      },
+      h4: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 20,
+      },
+      h5: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 16,
+      },
+      h6: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 14,
+      },
+    }
+  }
+};
+
+//context for color modes
+export const ColorModeContext = createContext({
+  toggleColorMode: () => { }
+});
+
+export const useMode = () => {
+  const [mode, setMode] = useState("dark");
+
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prev) => (prev === "dark" ? "light" : "dark"))
+      }
+    }), []
+  );
+
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  return [theme, colorMode];
+}
